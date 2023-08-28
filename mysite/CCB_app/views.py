76e603919cac1f023100bc7816e3d9e014a05c4b
Travis Ditmanson <travisditmanson@gmail.com>
2023-08-28 16:51:31 -0600

    hello CCB

--- mysite/CCB_app/views.py
+++ mysite/CCB_app/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('<h1> CCB </h1> \n <h3> Clydes Coding Buisness </h3>')
+    return HttpResponse('<h1> CCB </h1> \n <h3> Clydes Coding Buisness</h3>')
