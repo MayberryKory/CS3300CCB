9a83b7c463b89fa48168aae5004e37328216848d
Kroleski <rkroleski@comcast.net>
2023-08-29 12:39:07 -0600

    I'm still trying... - bob

--- ourEnv/main.py
+++ ourEnv/main.py
@@ -5,5 +5,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('VIM FOR LIFE')
     print("VIM is for massochists...")
+    print("Bob's second solo attempt to do something right.")
 
 
