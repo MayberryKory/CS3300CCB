cadfae4cea03dfd4fff380c910f17c2ac83a3489
MayberryKory <102997911+MayberryKory@users.noreply.github.com>
2023-08-29 12:51:36 -0600

    Removing Virtual Environment

--- ourEnv/helloworldproject/helloworldproject/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from helloWorld.views import hello_world
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('hello/', hello_world)
-]
